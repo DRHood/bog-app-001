435127312ea6aaf492f8e94d27ed614e2deeb6f4
Darryl Hood <darryl.r.hood@gmail.com>
2020-03-10 14:56:00 -0400

    made create one route

--- controllers/creature.js
+++ controllers/creature.js
@@ -3,14 +3,23 @@ const Creature = require('../models/creature.js');
 
 const creatureRouter = express.Router();
 
+// Get all
 creatureRouter.get('/', (req, res) => {
   Creature.find().then((creatures) => {
     res.json();
   });
 });
 
-creatureRouter.get('/:id', (req, res) =>{
+// Get one
+creatureRouter.get('/:creatureId', (req, res) => {
   Creature.findById(req.params.id).then(() => {
+    res.json();
+  });
+});
+
+// Create one
+creatureRouter.post('/', (req, res) => {
+  Creature.create(req.body).then(() => {
     res.json();
   });
 });
