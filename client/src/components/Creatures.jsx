709717ab0af21424131283667bdb2accdc689c5a
Darryl Hood <darryl.r.hood@gmail.com>
2020-03-11 09:13:44 -0400

    updating Creatures component, because I forgot

--- client/src/components/Creatures.jsx
+++ client/src/components/Creatures.jsx
@@ -1,7 +1,58 @@
 import React, { Component } from 'react'
-import Creature from '../../../models/Creature'
+import axios from 'axios'
+import { Link } from 'react-router-dom'
 
 export default class Creatures extends Component {
+
+    state = {
+        creatures: [],
+        newCreature: {
+            name: '',
+            description: ''
+        },
+        isNewFormDisplayed: false
+    }
+
+    componentDidMount() {
+        this.getAllCreatures()
+     }
+
+     getAllCreatures = () => {
+        axios.get('/api/creatures')
+            .then((res) => {
+                this.setState({creatures: res.data})
+            })
+    }
+
+    handleNewFormChange = (event) => {
+        const copiedCreature = {...this.state.newCreature}
+        copiedCreature[event.target.name] = event.target.value
+
+        this.setState({newCreature: copiedCreature})
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault()
+
+        axios.post('/api/creatures', this.state.newCreature)
+            .then(() => {
+                this.getAllCreatures()
+                this.setState({
+                    isNewFormDisplayed: false,
+                    newCreature: {
+                        name: '',
+                        description: ''
+                    }
+                })
+            })
+    }
+
+    handleToggleNewForm = () => {
+        this.setState((state) => {
+            return {isNewFormDisplayed: !state.isNewFormDisplayed}
+        })
+    }
+
     render() {
         return (
             <div>
